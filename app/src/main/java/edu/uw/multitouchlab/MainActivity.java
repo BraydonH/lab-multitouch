01c63a202241666cc3a903ade7bbc0ecfdeab840
BraydonHall <BraydonH@cs.washington.edu>
2017-11-20 19:39:18 -0800

    part 1

--- app/src/main/java/edu/uw/multitouchlab/MainActivity.java
+++ app/src/main/java/edu/uw/multitouchlab/MainActivity.java
@@ -2,6 +2,7 @@ package edu.uw.multitouchlab;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.MotionEvent;
 
 public class MainActivity extends AppCompatActivity {
@@ -24,9 +25,14 @@ public class MainActivity extends AppCompatActivity {
         float y = event.getY() - getSupportActionBar().getHeight(); //closer to center...
 
         int action = event.getActionMasked();
+
+        int index = event.getActionIndex();
+
         switch(action) {
             case (MotionEvent.ACTION_DOWN) : //put finger down
                 //Log.v(TAG, "finger down");
+                Log.v(TAG, "Pointer Index: " + index);
+                Log.v(TAG, "Pointer ID:" + event.getPointerId(index));
                 view.ball.cx = x;
                 view.ball.cy = y;
                 return true;
@@ -36,6 +42,9 @@ public class MainActivity extends AppCompatActivity {
                 view.ball.cy = y;
                 return true;
             case (MotionEvent.ACTION_UP) : //lift finger up
+                Log.v(TAG, "Pointer Index: " + index);
+                Log.v(TAG, "Pointer ID:" + event.getPointerId(index));
+                return true;
             case (MotionEvent.ACTION_CANCEL) : //aborted gesture
             case (MotionEvent.ACTION_OUTSIDE) : //outside bounds
             default :
